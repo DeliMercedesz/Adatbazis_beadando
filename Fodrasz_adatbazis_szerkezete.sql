865b589f6c17da3c7b57991ca2eabba419fa1df8
szanna04 <153095392+szanna04@users.noreply.github.com>
2024-05-07 13:10:12 +0200

    Update Fodrasz_adatbazis_szerkezete.sql

--- Fodrasz_adatbazis_szerkezete.sql
+++ Fodrasz_adatbazis_szerkezete.sql
@@ -68,7 +68,7 @@ ALTER TABLE foglalasok
 
 INSERT INTO foglalasok (foglalas_id, idopont, ugyfel_fk, fodrasz_fk, kezdes, befejezes, fizetve, fizetesi_mod, megjegyzes)
 VALUES 
-(1, '2024-05-10 10:00', 3, 15, '2024-05-10 10:00', '2024-05-10 11:30', 'N', 'Nincs', '')
+    (1, '2024-05-10 10:00', 3, 15, '2024-05-10 10:00', '2024-05-10 11:30', 'N', 'Nincs', '')
     (2, '2024-05-21 08:00', 16, 4, '2024-05-21 08:05', '2024-05-21 09:35', 'I', 'Bankkártya', ‘Freestyle kezelés')
     (3, '2024-05-04 13:00', 35, 2, '2024-05-04 13:00', '2024-05-04 14:30', 'N', 'Nincs', Gyors kezelés')
     (4, '2024-05-02 08:00', 33, 10, '2024-05-02 08:04', '2024-05-02 09:34', 'I', 'Bankkártya', 'Időpont: 2024-05-02 08:04 – Gyors kezelés')
@@ -128,8 +128,7 @@ VALUES
     (58, '2024-05-25 09:00', 17, 3, '2024-05-25 09:00', '2024-05-25 10:30', 'N', 'Nincs', '')
     (59, '2024-05-18 13:00', 8, 2, '2024-05-18 13:01', '2024-05-18 14:31', 'I', 'Bankkártya', '')
     (60, '2024-05-02 14:00', 10, 7, '2024-05-02 14:09', '2024-05-02 15:39', 'I', 'Készpénz', '')
-
-
+    
 --Szolgaltatasok tabla letrehozasa--
 
 CREATE TABLE szolgaltatasok (
